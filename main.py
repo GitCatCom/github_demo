d4bf22fa662e574e70fe287bd96fbcba6fd1f78b
GitQueen <3602332036@qq.com>
2018-06-05 08:45:25 +0800

    Change some codes.
    修改添加一些代码。

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import time
 
-print("HelloGithub.")
-print("The current date is {}.".format(time.asctime()))
+def simple_func():
+    for _ in range(1, 10):
+        print('*'*_)
+
+if __name__ == '__main__':
+    print("HelloGithub.")
+    print("The current date is {}.".format(time.asctime()))
+    simple_func()
